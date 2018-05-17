c109dc9f9b250467fab5a82681c63df9102f806f
AdamKaj <akajzer2@aol.com>
2018-05-17 17:30:18 -0400

    abstract class for the actual civ itself, not the leader

---
+++ eclipse-workspace/Civilization/src/Civilization.java
@@ -0,0 +1,14 @@
+
+/**
+ * @author AdamKaj
+ * 
+ *
+ */
+public abstract class Civilization {
+	protected double science;
+	protected double production;
+	protected double happiness;
+	protected double gold;
+	protected String[] cityNames;
+	protected 
+}
