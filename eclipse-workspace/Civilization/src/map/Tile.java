8c1135396d3e385cab6e27ec98ff23d0810bed1d
mnuryga <mnuryga@yahoo.com>
2018-05-17 18:31:05 -0400

    I create the abstract Tile class

--- eclipse-workspace/Civilization/src/map/Tile.java
+++ eclipse-workspace/Civilization/src/map/Tile.java
@@ -1,5 +1,16 @@
 package map;
 
-public abstract class Tile {
+import leaders.Leader;
 
+public abstract class Tile {
+	protected int terrainID;
+	protected int movesRequired;
+	protected double productionBase;
+	protected double productionPotential;
+	protected double foodBase;
+	protected double foodPotential;
+	protected double goldBase;
+	protected double goldPotential;
+	protected boolean crossable;
+	protected Leader owner;
 }
