60a0bbb7f8e18c4d1c22eeae1b87deba307aa0bc
AdamKaj <akajzer2@aol.com>
2018-05-17 17:25:50 -0400

    # WARNING: head commit changed in the meantime
    h
    
    test leader

---
+++ eclipse-workspace/Civilization/src/Leaders/Leader.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package Leaders;
+
+/**
+ * @author akajz
+ *
+ */
+public abstract class Leader {
+	protected String civName;
+	protected String leaderName;
+	protected String civNamePlural;
+	protected String[] cityNames;
+	
+	
+}
