ed00ad66e1d218abd6f9a107352546e823b19192
mnuryga <mnuryga@yahoo.com>
2018-05-17 21:52:24 -0400

    Made the GrassTile constructor

--- eclipse-workspace/Civilization/src/map/GrassTile.java
+++ eclipse-workspace/Civilization/src/map/GrassTile.java
@@ -1,5 +1,20 @@
 package map;
 
-public class GrassTile {
+import leaders.Leader;
 
+public class GrassTile extends Tile{	
+	public GrassTile(int tterrainID, int tmovesRequired, double tproductionBase, double tproductionPotential, double tfoodBase,
+			double tfoodPotential, double tgoldBase, double tgoldPotential, boolean tcrossable, boolean toccupied, Leader towner) {
+		super.terrainID = tterrainID;
+		super.movesRequired = tmovesRequired;
+		super.productionBase = tproductionBase;
+		super.productionPotential = tproductionPotential;
+		super.foodBase = tfoodBase;
+		super.foodPotential = tfoodPotential;
+		super.goldBase = tgoldBase;
+		super.goldPotential = tgoldPotential;
+		super.crossable = tcrossable;
+		super.occupied = toccupied;
+		super.owner = towner;
+	}
 }
