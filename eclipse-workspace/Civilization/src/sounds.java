3f1a5b2e99c03a9e8b583834f577ce3977a66e06
jeffrey.bengiovanni <jeffrey.bengiovanni@10.243.64.142>
2018-05-18 11:48:07 -0400

    Made sound files protected

--- eclipse-workspace/Civilization/src/sounds.java
+++ eclipse-workspace/Civilization/src/sounds.java
@@ -8,8 +8,8 @@ import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.UnsupportedAudioFileException;
 
 class sounds {
-	private File musicfile = new File("");
-	private File clickfile = new File("");
+	protected File musicfile = new File("");
+	protected File clickfile = new File("");
 
 	public void clickPlay() throws UnsupportedAudioFileException, IOException, LineUnavailableException {	
 		AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(clickfile);
