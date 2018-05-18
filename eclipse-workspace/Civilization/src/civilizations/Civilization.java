29ff40ea1c45a9fd858b245db0565146e32707bc
AdamKaj <akajzer2@aol.com>
2018-05-17 20:51:14 -0400

    Further Creation of Classes
    - Added City.java
    - Worked on Civilization.java
    - Created technologies package
    - Added Technology.java

--- eclipse-workspace/Civilization/src/civilizations/Civilization.java
+++ eclipse-workspace/Civilization/src/civilizations/Civilization.java
@@ -1,6 +1,14 @@
 package civilizations;
 
-public class Civilization {
+import java.util.ArrayList;
+
+import technology.Technology;
+
+public abstract class Civilization {
 	protected double goldReserve;
 	protected double goldPerTurn;
+	protected ArrayList <City> cities;
+	protected ArrayList <Technology> technologies;
+	
+	
 }
