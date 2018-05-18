95d9268ff112ba85354cadeb6d98e61c9efb6b7b
jeffrey.bengiovanni <jeffrey.bengiovanni@10.243.64.142>
2018-05-18 11:34:55 -0400

    Added setup for sounds

--- eclipse-workspace/Civilization/src/sounds.java
+++ eclipse-workspace/Civilization/src/sounds.java
@@ -1,1 +1,26 @@
+import java.io.File;
+import java.io.IOException;
 
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+
+class sounds {
+	private File musicfile = new File("");
+	private File clickfile = new File("");
+	
+public void clickPlay() throws UnsupportedAudioFileException, IOException, LineUnavailableException {	
+		AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(clickfile);
+		Clip clip = AudioSystem.getClip();
+		clip.open(audioInputStream);
+		clip.start();
+	}
+public void musicPlay() throws UnsupportedAudioFileException, IOException, LineUnavailableException {	
+	AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(clickfile);
+	Clip clip = AudioSystem.getClip();
+	clip.open(audioInputStream);
+	clip.start();
+}
+}
