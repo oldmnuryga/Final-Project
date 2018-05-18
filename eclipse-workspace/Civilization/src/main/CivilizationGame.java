eb27529678f30068430b82e37bdf3b5870c3f850
Matthew Uryga <mnuryga@yahoo.com>
2018-05-18 11:14:09 -0400

    Fixed Spacing

--- eclipse-workspace/Civilization/src/main/CivilizationGame.java
+++ eclipse-workspace/Civilization/src/main/CivilizationGame.java
@@ -1,6 +1,6 @@
 package main;
 
 public class CivilizationGame {
-  public CivilizationGame(){
-  }
+	public CivilizationGame(){
+	}
 }
--- eclipse-workspace/Civilization/src/map/Tile.java
+++ eclipse-workspace/Civilization/src/map/Tile.java
@@ -15,89 +15,89 @@ public abstract class Tile {
 	protected boolean occupied;
 	protected boolean improved;
 	protected Leader owner;
-	
+
 	//GETTERS AND SETTERS
-	
+
 	public int getTerrainID() {
 		return terrainID;
 	}
-	
+
 	public void setTerrainID(int terrainID) {
 		this.terrainID = terrainID;
 	}
-	
+
 	public int getMovesRequired() {
 		return movesRequired;
 	}
-	
+
 	public void setMovesRequired(int movesRequired) {
 		this.movesRequired = movesRequired;
 	}
-	
+
 	public double getProductionBase() {
 		return productionBase;
 	}
-	
+
 	public void setProductionBase(double productionBase) {
 		this.productionBase = productionBase;
 	}
-	
+
 	public double getProductionPotential() {
 		return productionPotential;
 	}
-	
+
 	public void setProductionPotential(double productionPotential) {
 		this.productionPotential = productionPotential;
 	}
-	
+
 	public double getFoodBase() {
 		return foodBase;
 	}
-	
+
 	public void setFoodBase(double foodBase) {
 		this.foodBase = foodBase;
 	}
-	
+
 	public double getFoodPotential() {
 		return foodPotential;
 	}
-	
+
 	public void setFoodPotential(double foodPotential) {
 		this.foodPotential = foodPotential;
 	}
-	
+
 	public double getGoldBase() {
 		return goldBase;
 	}
-	
+
 	public void setGoldBase(double goldBase) {
 		this.goldBase = goldBase;
 	}
-	
+
 	public double getGoldPotential() {
 		return goldPotential;
 	}
-	
+
 	public void setGoldPotential(double goldPotential) {
 		this.goldPotential = goldPotential;
 	}
-	
+
 	public boolean isCrossable() {
 		return crossable;
 	}
-	
+
 	public void setCrossable(boolean crossable) {
 		this.crossable = crossable;
 	}
-	
+
 	public boolean isOccupied() {
 		return occupied;
 	}
-	
+
 	public void setOccupied(boolean occupied) {
 		this.occupied = occupied;
 	}
-	
+
 	public boolean isImproved() {
 		return improved;
 	}
@@ -109,7 +109,7 @@ public abstract class Tile {
 	public Leader getOwner() {
 		return owner;
 	}
-	
+
 	public void setOwner(Leader owner) {
 		this.owner = owner;
 	}
