1912f72e174d851b9c1ec9c790bbe6b6e0487902
mnuryga <mnuryga@yahoo.com>
2018-05-20 21:29:35 -0400

    Started working on graphics

--- eclipse-workspace/Civilization/src/main/CivilizationGame.java
+++ eclipse-workspace/Civilization/src/main/CivilizationGame.java
@@ -1,7 +1,36 @@
 package main;
 
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+
 public class CivilizationGame {
+	//JGRAPHICS CONSTRUCTORS
+	JFrame frame = new JFrame("Civilization");
+	JScrollPane mapPane = new JScrollPane();
+	JPanel leftPanel = new JPanel(); //200 from right
+	JPanel topPanel = new JPanel(); //50 from top
+	
+	
 	public CivilizationGame(){
+		//ADD STUFF
+	}
+	
+	public void display(){
+		//JFrame
+		frame.setPreferredSize(new Dimension(1920, 1080));
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLayout(null);
+		frame.setVisible(true);
+		frame.setResizable(false);
 		
+		//mapPane
+		mapPane.setBounds(200, 50, 1720, 1030);
+		
+		
+		
+		frame.pack();
 	}
 }
--- eclipse-workspace/Civilization/src/main/civilizationMain.java
+++ eclipse-workspace/Civilization/src/main/civilizationMain.java
@@ -3,5 +3,6 @@ package main;
 public class civilizationMain {
 	public static void main(String[] args) {
 		CivilizationGame c = new CivilizationGame();
+		c.display();
 	}
 }
