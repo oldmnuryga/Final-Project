7f052220a7a86f288d876ccfc607b7bc93d7e822
AdamKaj <akajzer2@aol.com>
2018-05-21 12:59:39 -0400

    Created Aqueduct object

--- eclipse-workspace/Civilization/src/civilizations/buildings/Aqueduct.java
+++ eclipse-workspace/Civilization/src/civilizations/buildings/Aqueduct.java
@@ -1,1 +1,21 @@
+package civilizations.buildings;
 
+import civilizations.Building;
+import civilizations.City;
+
+public class Aqueduct extends Building{
+	protected String name; 
+	protected String description;
+	protected double productionRequirement;
+	protected int goldMaintenance;
+	protected int techRequired;
+	protected City location;
+	public Aqueduct (City city) {
+		name = "Aqueduct";
+		description = "Allows City to Grow over 10.";
+		productionRequirement = 120;
+		goldMaintenance = 2;
+		techRequired = 12;
+		city.setCitizenCap(30);
+	}
+}
