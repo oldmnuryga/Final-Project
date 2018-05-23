62dc6d91b8065659f9f6959160b2ec8546213145
AdamKaj <akajzer2@aol.com>
2018-05-23 09:26:28 -0400

    inheritance fix in buildings and wonders

--- eclipse-workspace/Civilization/src/civilizations/buildings/ManufacturingPlant.java
+++ eclipse-workspace/Civilization/src/civilizations/buildings/ManufacturingPlant.java
@@ -1,1 +1,60 @@
+package civilizations.buildings;
+
+import civilizations.Building;
+import civilizations.City;
+
+public class ManufacturingPlant extends Building{
+	public ManufacturingPlant (City city) {
+		location = city;
+		name = "Manufacturing Plant";
+		description = "Production +100%.";
+		productionRequirement = 320;
+		goldMaintenance = 6;
+		techRequired = 61;
+		city.setProductionRate(city.getProductionRate()*2.0);
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public double getProductionRequirement() {
+		return productionRequirement;
+	}
+	public void setProductionRequirement(double productionRequirement) {
+		this.productionRequirement = productionRequirement;
+	}
+	public int getGoldMaintenance() {
+		return goldMaintenance;
+	}
+	public void setGoldMaintenance(int goldMaintenance) {
+		this.goldMaintenance = goldMaintenance;
+	}
+	public int getTechRequired() {
+		return techRequired;
+	}
+	public void setTechRequired(int techRequired) {
+		this.techRequired = techRequired;
+	}
+	public City getLocation() {
+		return location;
+	}
+	public void setLocation(City location) {
+		this.location = location;
+	}
+}
+
+
+
+
+
+
+
 
