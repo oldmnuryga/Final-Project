ee8207d84336f82202ae965f2ca94105fc2b303e
jeffrey.bengiovanni <jeffrey.bengiovanni@10.243.64.142>
2018-05-29 08:24:42 -0400

    Edited bomber unit

--- eclipse-workspace/Civilization/src/units/Bomber.java
+++ eclipse-workspace/Civilization/src/units/Bomber.java
@@ -1,1 +1,17 @@
+package units;
 
+public class Bomber extends Unit {
+
+	public Bomber() {
+		super.hitpoints = 130;
+		super.attackRating = 75;
+		super.productionCost = 375;
+		super.location  = null;
+		super.ranged = true;
+		super.alive = true;
+		super.fortified = false;
+		super.maxMovement = 4;
+		super.maintenance = 4;
+		super.isAir = true;
+	}
+}
