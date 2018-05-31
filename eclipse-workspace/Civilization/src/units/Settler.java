3df27b623e05d139ba5bb3723d47357916f544d7
JJPickle <39377277+JJPickle@users.noreply.github.com>
2018-05-31 11:38:49 -0400

    Create Settler.java

---
+++ eclipse-workspace/Civilization/src/units/Settler.java
@@ -0,0 +1,21 @@
+package units;
+
+public class Settler extends Unit {
+
+	public Settler() {
+		super.hitpoints = 15;
+		super.attackRating = 0;
+		super.productionCost = 106;
+		super.location  = null;
+		super.ranged = false;
+		super.alive = true;
+		super.fortified = false;
+		super.maxMovement = 2;
+		super.maintenance = 1;
+		super.isAir = false;		
+		super.isGround = true;
+		super.isNaval = false;
+		super.isSelected = false;
+		super.unitName = "Settler";
+	}
+}
