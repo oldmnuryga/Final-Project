cec5ff7f6316024e83877a9d565d8b4fb5b0d5d0
AdamKaj <39377108+AdamKaj@users.noreply.github.com>
2018-06-13 10:15:19 -0400

    Update Road.java

--- eclipse-workspace/Civilization/src/improvements/Road.java
+++ eclipse-workspace/Civilization/src/improvements/Road.java
@@ -5,7 +5,7 @@ public class Road {
 	protected int goldMaintenance;
 	protected int timeToBuild;
 
-	//all improvements would be built by the worker, if said worker existed in the game
+//all improvements would be built by the worker onto a tile, if said worker was implemented in the game
 	//increases mobility of the player
 	
 	public Road() {
